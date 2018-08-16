0b45f9ee0b207c790d1e55e62da09e8f07cf4d76
preetsimer <simerpreet1851.cse16@chitkara.edu.in>
2018-08-16 22:55:05 +0530

    Assignment-3

---
+++ assignment-3.py
@@ -0,0 +1,30 @@
+#question 1
+n=int(input("Enter size of list "))
+l=[]
+print("Enter list elements ")
+for i in range(1,n+1):
+    a=input()
+    l.append(a)
+print(l)
+#question 2
+print("Merged List ")
+l2=["google","apple","facebook","microsoft","tesla"]
+l=l+l2
+print(l)
+#question 3
+e=input("Enter element to account its occurrence :")
+c=l.count(e)
+print(c)
+#question 4
+n=int(input("Enter size of list "))
+l=[]
+print("Enter list elements ")
+for i in range(1,n+1):
+    a=input()
+    l.append(a)
+l.sort()
+print("The sorted list is ")
+print(l)
+#question 5
+    
+      
